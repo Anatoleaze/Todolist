c9e08675c04b7ecac993376a0e0e603bcc7562c5
Anatole Aze <anatoleaze@hotmail.fr>
2022-02-25 18:50:29 +0100

    Add all Project

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+/*import firebase from 'firebase';
+
+const firebaseConfig={
+    apiKey: "AIzaSyBel7-EvNegprr8OtBV8-WeAVnBSk9CuR0",
+    authDomain: "todo-list-9443e.firebaseapp.com",
+    projectId: "todo-list-9443e",
+    storageBucket: "todo-list-9443e.appspot.com",
+    messagingSenderId: "496647521136",
+    appId: "1:496647521136:web:48a9addcece3776fdb05b5",
+    measurementId: "G-7M80E2WNX3",
+};
+
+export default firebase.initializeApp(firebaseConfig);
+*/
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPIOI02BV_cIFN3P4zFT7ROzXPKhY9KgM",
+  authDomain: "my-todolist-app-6017f.firebaseapp.com",
+  projectId: "my-todolist-app-6017f",
+  storageBucket: "my-todolist-app-6017f.appspot.com",
+  messagingSenderId: "868360892336",
+  appId: "1:868360892336:web:1fdb838c37750623beb075",
+};
+
+export default firebase.initializeApp(firebaseConfig);
