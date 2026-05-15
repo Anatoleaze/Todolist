f55b29b75f2dbabd3ee3c935deaf3589ff8c0b01
AnatoleA <anatoleaze@hotmail.fr>
2026-05-15 21:43:04 +0200

    add env variable

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPIOI02BV_cIFN3P4zFT7ROzXPKhY9KgM",
-  authDomain: "my-todolist-app-6017f.firebaseapp.com",
-  projectId: "my-todolist-app-6017f",
-  storageBucket: "my-todolist-app-6017f.appspot.com",
-  messagingSenderId: "868360892336",
-  appId: "1:868360892336:web:1fdb838c37750623beb075"
-};
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 
